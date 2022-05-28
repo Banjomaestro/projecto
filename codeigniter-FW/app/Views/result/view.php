33757941e65a15a063aebd3c9eeeaa8d923a8664
Pauline Gobé <pauline.gobe16@gmail.com>
2022-05-28 13:28:07 +0200

    css okay

--- codeigniter-FW/app/Views/result/view.php
+++ codeigniter-FW/app/Views/result/view.php
@@ -4,18 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Result</title>
+    <link rel="stylesheet" type="text/css" href="/CSS/stylesRes.css" type="text/css">
+    <title>Resultat</title>
 </head>
 <body>
-    <h2>Voici ton résultat, ton patronus est : </h2> 
-    <p><?php echo $Prenom?></p>
-    <p><?php echo $Nom?> </p>
-    
-    <p><img src="<?=URL?>images/prof/<?=$ID_prof?>.jpg" alt="photo de prof"></p>
-    
-    <p><?php echo $Description?></p>
-    <!-- <img src="/public/images/<?php $Nom ?>.jpg" alt=""> -->
+    <div class = "head">
+        <h1>Découvre ton patronus !</h1>
+    </div>
+    <div class = "titre">
+        <h2>Voici ton résultat, ton patronus est : </h2> 
+    </div>
 
+    <div class = "container">
+        <div class = "result">
+            <p><?php echo $Prenom?></p>
+            <p><?php echo $Nom?> </p>
+        </div>
+        
+        
+        <p><img src="<?=URL?>images/prof/<?=$ID_prof?>.jpg" alt="photo de prof"></p>
+        
+        <p><?php echo $Description?></p>
+        <!-- <img src="/public/images/<?php $Nom ?>.jpg" alt=""> -->
+    </div>
     <a href="/start">Retour à l'accueil</a>
 </body>
 </html>
--- codeigniter-FW/app/Views/start/indexNotDone.php
+++ codeigniter-FW/app/Views/start/indexNotDone.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="/CSS/stylesRes.css" type="text/css">
     <title>Accueil</title>
 </head>
 <body>
-    <div><h1>Découvre qui est ton patronus !</h1></div>
-    <div>
+    <div class= "head"><h1>Découvre qui est ton patronus !</h1></div>
+    <div class = "titre">
         <h2>Bienvenue ! Commence le quizz pour découvrir quel prof de l'IMAC te correspond le plus !</h2>
-        <a href="Quiz">start quizz</a>
+        <a href="Quiz">Commencer !</a>
     </div>
     
 </body>
